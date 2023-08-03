8ed38e631d6e9ad5cb28b53f0983f5c9fa1b6756
mayue <mayue@mayues-MacBook-Pro.local>
2023-08-02 23:01:54 -0400

    customhook usehttp

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,36 @@
+
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+import useHttp from '../../hooks/useHttp';
+
+
+const NewTask = (props) => {
+  const {isLoading, error, sendRequest: sendTaskRequest} = useHttp();
+
+
+  const enterTaskHandler = async (taskText) => {
+    const createTask = (taskData) => {
+      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+  
+      props.onAddTask(createdTask);
+    };
+
+    sendTaskRequest({url:'https://customhook-533d5-default-rtdb.firebaseio.com/tasks.json',
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json',
+     },
+    body: {text: taskText}
+    }, createTask);
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
